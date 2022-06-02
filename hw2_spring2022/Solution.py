d8dc32abda45f77b77c8d1993037222edf9ef200
YoavPerez <49918755+YoavPerez@users.noreply.github.com>
2022-06-02 10:19:26 +0300

    creating the tables(without views)

--- hw2_spring2022/Solution.py
+++ hw2_spring2022/Solution.py
@@ -1,18 +1,45 @@
 from typing import List
-import Utility.DBConnector as Connector
-from Utility.Status import Status
-from Utility.Exceptions import DatabaseException
-from Business.File import File
-from Business.RAM import RAM
-from Business.Disk import Disk
+import hw2_spring2022.Utility.DBConnector as Connector
+from hw2_spring2022.Utility.Status import Status
+from hw2_spring2022.Utility.Exceptions import DatabaseException
+from hw2_spring2022.Business.File import File
+from hw2_spring2022.Business.RAM import RAM
+from hw2_spring2022.Business.Disk import Disk
 from psycopg2 import sql
 
 
 # create the main tables from the parameters.
 def create_table(table_params: dict):
-    init_query = "BEGIN; CREATE TABLE "
-    # add the paramteres to the query and perform it.
-    pass
+    query = "CREATE TABLE "
+    # add the parameters to the query and perform it.
+    query += table_params["table name"] + "\n("
+
+    for field in table_params["fields"]:
+        field_line = "{name} {kind} {check}, \n".format(name=field["name"], kind=field["type"], check=field["check"])
+        query += field_line
+
+    query += "PRIMARY KEY ("
+    for i, prime in enumerate(table_params["primaries"]):
+        if i < len(table_params["primaries"]) - 1:
+            query += prime + ", "
+        else:
+            query += prime
+    query += ")"
+
+    if "foreign" in table_params.keys():
+        query += ", \n"
+        for i, fk in enumerate( table_params["foreign"]):
+
+            query += "FOREIGN KEY ({key})\n REFERENCES {table} ({orig_f})\n ON DELETE CASCADE".format(
+                key=fk["name"], table=fk["orig table"], orig_f=fk["orig name"]
+            )
+            if i < len(table_params["foreign"])-1:
+                query += ",\n"
+            else:
+                query += "\n"
+    else: query += "\n"
+    query += ");\n"
+    return query
 
 
 # the difference is the outline of the dictionary, can be in the function above but it is quite confusing to my opinion.
@@ -22,61 +49,68 @@ def create_multi_valued_tables(mv_table_params: dict):
 
 def createTables():
     # creating tables and designing the database
-    file_table_params = {"table name": "File",
-                         "primary": {"name": "file_id", "type":"INTEGER", "check": "CHECK(id > 0)"},
-                         "fields": [
-                             {"name": "type", "type":"TEXT","check":"NOT NULL"},
-                             {"name": "file_size", "type":"INTEGER", "check":"NOT NULL CHECK(size >= 0)"},
-                            ]
-                         }
-    disk_table_params = {"table name": "Disk",
-                         "primary": {"name": "disk_id", "type":"INTEGER", "check": "CHECK(id > 0)"},
-                         "fields": [
-                             {"name": "company", "type":"TEXT","check":"NOT NULL"},
-                             {"name": "speed", "type":"INTEGER", "check":"NOT NULL CHECK(speed > 0)"},
-                             {"name": "space", "type":"INTEGER", "check":"NOT NULL CHECK(space >= 0)"},
-                             {"name": "cost_per_byte", "type": "INTEGER", "check":"NOT NULL CHECK (cost_per_byte > 0)"}
-                            ]
-                         }
-    ram_table_params = {"table name": "Ram",
-                        "primary": "ram_id", "type": "INTEGER", "check": "CHECK(id > 0)",
-                        "fields": [
-                            {"name": "ram_size", "type": "INTEGER", "check": "NOT NULL CHECK(space > 0)"},
+    query = "BEGIN;"
+
+    # PARAMETERS FOR CREATING THE FILES TABLE
+    files_table = {"table name": "File", "fields": [
+        {"name": "id", "type": "INTEGER", "check": "CHECK (id > 0)"},
+        {"name": "type", "type": "TEXT", "check": "NOT NULL"},
+        {"name": "size", "type": "INTEGER", "check": "NOT NULL CHECK (size >= 0)"}
+    ], "primaries": ["id"]}
+
+    # PARAMETERS FOR CREATING THE DISKS TABLE
+    disks_table = {}
+    disks_table["table name"] = "Disk"
+    disks_table["fields"] = [
+                            {"name": "id", "type": "INTEGER", "check": "CHECK (id > 0)"},
+                            {"name": "company", "type": "TEXT", "check": "NOT NULL"},
+                            {"name": "speed", "type": "INTEGER", "check": "NOT NULL CHECK (speed > 0)"},
+                            {"name": "space", "type": "INTEGER", "check": "NOT NULL CHECK (space >= 0)"},
+                            {"name": "cost_per_byte", "type": "INTEGER", "check": "NOT NULL CHECK (cost_per_byte > 0)"}
+                        ]
+    disks_table["primaries"] = ["id"]
+
+    # PARAMETERS FOR CREATING THE RAMS TABLE
+    rams_table = {}
+    rams_table["table name"] = "Ram"
+    rams_table["fields"] = [
+                            {"name": "id", "type": "INTEGER", "check": "CHECK (id > 0)"},
+                            {"name": "size", "type": "INTEGER", "check": "NOT NULL CHECK (size  > 0)"},
                             {"name": "company", "type": "TEXT", "check": "NOT NULL"}
-                            ]
-                        }
-    files_on_disks_table_params = {"table name": "FilesOnDisks",
-                         "primary": {"name": "file_id", "type": "INTEGER", "check": "CHECK(id > 0)",
-                                 "name": "disk_id", "type":"INTEGER", "check": "CHECK(id > 0)"},
-                         "fields": [
-                         ]
-                         }
-    rams_on_disks_table_params = {"table name": "RamsOnDisks",
-                        "primary": {"name": "ram_id", "type": "INTEGER", "check": "CHECK(id > 0)",
-                                               "name": "disk_id", "type": "INTEGER", "check": "CHECK(id > 0)"},
-                        "fields": [
-                                   ]
-                        }
+                        ]
+    rams_table["primaries"] = ["id"]
 
-    create_table(file_table_params)
-    create_table(disk_table_params)
-    create_table(ram_table_params)
-    create_table(files_on_disks_table_params)
-    create_table(rams_on_disks_table_params)
+    # PARAMETERS FOR CREATING THE MULTI VALUED FILES FOR DISKS TABLE
+    disk_mv_files = {}
+    disk_mv_files["table name"] = "FilesOnDisks"
+    disk_mv_files["fields"] = [{"name": "file_id", "type": "INTEGER", "check": ""},
+                               {"name": "disk_id", "type": "INTEGER", "check": ""}]
+    disk_mv_files["primaries"] = ["file_id", "disk_id"]
+    disk_mv_files["foreign"] = [{"name": "disk_id", "orig table": "Disk", "orig name": "id"},
+                                               {"name": "file_id", "orig table": "File", "orig name": "id"}]
 
-    # the files and ram on the disks are multivalued so we need tables for them, with disk.id as foreign key
-    # TODO: understand how it works with CASCADE, FOREIGN KEYS REFERENCES, and ON DELETE
-    # TODO: general outline for multivalued dict:
-    mv_dict = {"name":"name",
-               "primary": "(key1, key2)",
-               "foreigns": [{"key":"key_name",
-                             "references":"table (field)"}]
-            }
+    # PARAMETERS FOR CREATING THE MULTI VALUED RAMS FOR DISKS TABLE
+    disk_mv_rams = {}
+    disk_mv_rams["table name"] = "RamsOnDisks"
+    disk_mv_rams["fields"] = [{"name": "ram_id", "type": "INTEGER", "check": ""},
+                                            {"name": "disk_id", "type": "INTEGER", "check": ""}]
+    
+    disk_mv_rams["primaries"] = ["ram_id", "disk_id"]
+    disk_mv_rams["foreign"] = [{"name": "disk_id", "orig table": "Disk", "orig name": "id"},
+                                              {"name": "ram_id", "orig table": "Ram", "orig name": "id"}]
+    
 
-    # TODO: do the same thing for VIEW to see how much space left and the total ram, but create query, not table
-    pass
+    # getting the contribution for the total query from each table
+    query += create_table(files_table)
+    query += create_table(disks_table)
+    query += create_table(rams_table)
+    query += create_table(disk_mv_files)
+    query += create_table(disk_mv_rams)
 
+    # TODO: add VIEW to help complex calculating
 
+    query += "COMMIT;"
+    return query
 
 def clearTables():
     pass
@@ -84,6 +118,7 @@ def clearTables():
 
 def dropTables():
     pass
+
 
 def runQuery(query):
     connector = None
@@ -117,7 +152,8 @@ def runCheckQuery(queryString) -> Status:
 
 def addFile(file: File) -> Status:
     query = sql.SQL("""INSERT INTO File VALUES({id}, {type}, {size})""").format(id=sql.Literal(file.getFileID()),
-    type=sql.Literal(file.getType()), size=sql.Literal(file.getSize()))
+                                                                                type=sql.Literal(file.getType()),
+                                                                                size=sql.Literal(file.getSize()))
     return runCheckQuery(query)
 
 
@@ -140,8 +176,8 @@ def deleteFile(file: File) -> Status:
 def addDisk(disk: Disk) -> Status:
     query = sql.SQL("""INSERT INTO Disk VALUES ({diskId},{company},
     {speed},{space},{cost})""").format(diskId=sql.Literal(disk.getDiskID()),
-    company=sql.Literal(disk.getCompany()),speed=sql.Literal(disk.getSpeed()),
-    space=sql.Literal(disk.getFreeSpace()),cost=sql.Literal(disk.getCost()))
+                                       company=sql.Literal(disk.getCompany()), speed=sql.Literal(disk.getSpeed()),
+                                       space=sql.Literal(disk.getFreeSpace()), cost=sql.Literal(disk.getCost()))
     return runCheckQuery(query)
 
 
@@ -161,8 +197,9 @@ def deleteDisk(diskID: int) -> Status:
 
 
 def addRAM(ram: RAM) -> Status:
-    query = sql.SQL("""INSERT INTO Ram VALUES ({ramId},{size},{company})""")\
-        .format(ramId=sql.Literal(ram.getRamID()),size=sql.Literal(ram.getSize()),company=sql.Literal(ram.getCompany()))
+    query = sql.SQL("""INSERT INTO Ram VALUES ({ramId},{size},{company})""") \
+        .format(ramId=sql.Literal(ram.getRamID()), size=sql.Literal(ram.getSize()),
+                company=sql.Literal(ram.getCompany()))
     return runCheckQuery(query)
 
 
@@ -184,18 +221,20 @@ def deleteRAM(ramID: int) -> Status:
 def addDiskAndFile(disk: Disk, file: File) -> Status:
     query = sql.SQL("""INSERT INTO Disk VALUES({diskId},{company},
        {disk_speed},{disk_space},{disk_cost});
-       INSERT INTO File in VALUES ({fileId},{type},{size});""")\
+       INSERT INTO File in VALUES ({fileId},{type},{size});""") \
         .format(diskId=sql.Literal(disk.getDiskID()),
-    company=sql.Literal(disk.getCompany()),disk_speed=sql.Literal(disk.getSpeed()),
-    disk_space=sql.Literal(disk.getFreeSpace()),disk_cost=sql.Literal(disk.getCost()),fileId=sql.Literal(file.getFileID()),
-    type=sql.Literal(file.getType()), size=sql.Literal(file.getSize()))
+                company=sql.Literal(disk.getCompany()), disk_speed=sql.Literal(disk.getSpeed()),
+                disk_space=sql.Literal(disk.getFreeSpace()), disk_cost=sql.Literal(disk.getCost()),
+                fileId=sql.Literal(file.getFileID()),
+                type=sql.Literal(file.getType()), size=sql.Literal(file.getSize()))
     return runCheckQuery(query)
 
 
 def addFileToDisk(file: File, diskID: int) -> Status:
-    query = sql.SQL("""INSERT INTO FilesOnDisks VALUES({fileId},{diskId}) WHERE disk_id IN (SELECT id AS disk_id FROM Disk WHERE id= {diskId} AND {file_size}<=space) AND id IN;
-    UPDATE Disk SET space=space-{file_size} WHERE id={diskId};""").format(diskId=sql.Literal(diskID),fileId=sql.Literal(file.getFileID()),
-    file_size=sql.Literal(file.getSize()))
+    query = sql.SQL("""INSERT INTO FilesOnDisks VALUES({fileId},{diskId}) WHERE id IN (SELECT id FROM Disk WHERE id= {diskId} AND {file_size}<=space);
+    UPDATE Disk SET space=space-{file_size} WHERE id={diskId};""").format(diskId=sql.Literal(diskID),
+                                                                          fileId=sql.Literal(file.getFileID()),
+                                                                          file_size=sql.Literal(file.getSize()))
     return runQuery(query)
 
 
@@ -210,46 +249,29 @@ def removeFileFromDisk(file: File, diskID: int) -> Status:
 def addRAMToDisk(ramID: int, diskID: int) -> Status:
     query = sql.SQL("""INSERT INTO RamsOnDisks VALUES({ramId},{diskId}) WHERE {diskId} IN (SELECT id FROM Disk WHERE id= {diskId});
     """).format(diskId=sql.Literal(diskID),
-    ramId=sql.Literal(ramID))
+                ramId=sql.Literal(ramID))
     return runCheckQuery(query)
 
 
 def removeRAMFromDisk(ramID: int, diskID: int) -> Status:
-    query = sql.SQL("""DELETE FROM RamsOnDisks WHERE disk_id = diskId AND ram_id = {ramId} ;""")\
-        .format(diskId=sql.Literal(diskID),ramId=sql.Literal(ramID))
+    query = sql.SQL("""DELETE FROM RamsOnDisks WHERE disk_id = diskId AND ram_id = {ramId} ;""") \
+        .format(diskId=sql.Literal(diskID), ramId=sql.Literal(ramID))
     return runCheckQuery(query)
 
 
 def averageFileSizeOnDisk(diskID: int) -> float:
-    query = sql.SQL("""SELECT AVG(size) FROM File WHERE id IN (SELECT file_id FROM FilesOnDisks WHERE disk_id = {diskId})""").format(diskId=sql.Literal(diskID))
-    result = runQuery(query)
-    return result
+    return 0
 
 
 def diskTotalRAM(diskID: int) -> int:
-    query = sql.SQL(
-        """SELECT SUM(size) FROM Ram WHERE id IN (SELECT ram_id FROM RamssOnDisks WHERE disk_id = {diskId})""").format(
-        diskId=sql.Literal(diskID))
-    result = runQuery(query)
-    return result
+    return 0
 
 
 def getCostForType(type: str) -> int:
-    # get the disks_id and files_id corresponding INNER JOIN to it the size of the files INNER JOIN
-    # the cost per bytes of the disks
-    #Multiply the two columns
-    query = sql.SQL(
-        """SELECT SUM(size*cost_per_byte) FROM (SELECT * FROM FilesOnDisk WHERE file_id in (SELECT id FROM File WHERE type={type_file}) INNER JOIN
-         (SELECT id AS file_id,size FROM File WHERE type={type_file}) 
-         INNER JOIN (SELECT id AS disk_id,cost_per_byte FROM Disk WHERE id IN 
-         (SELECT disk_id FROM FilesOnDisks WHERE file_id IN (SELECT id FROM Files WHERE type={type_file})) ))""").format(
-        type_file=sql.Literal(type))
-    result = runQuery(query)
-    return result
+    return 0
 
 
 def getFilesCanBeAddedToDisk(diskID: int) -> List[int]:
-
     return []
 
 
@@ -271,3 +293,27 @@ def mostAvailableDisks() -> List[int]:
 
 def getCloseFiles(fileID: int) -> List[int]:
     return []
+
+if __name__ == "__main__":
+    conn = None
+    try:
+        conn = Connector.DBConnector()
+        q = createTables()
+        print(q)
+        conn.execute(q)
+        conn.commit()
+    except DatabaseException.ConnectionInvalid as e:
+        print(e)
+    except DatabaseException.NOT_NULL_VIOLATION as e:
+        print(e)
+    except DatabaseException.CHECK_VIOLATION as e:
+        print(e)
+    except DatabaseException.UNIQUE_VIOLATION as e:
+        print(e)
+    except DatabaseException.FOREIGN_KEY_VIOLATION as e:
+        print(e)
+    except Exception as e:
+        print(e)
+    finally:
+        # will happen any way after try termination or exception handling
+        conn.close()
